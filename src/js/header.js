0842d97eac288717a7d6be63147ec374ffeb404f
thanhd <thanhdanh@icd-vn.com>
2022-05-31 11:50:16 +0700

    add flag icon

--- src/js/header.js
+++ src/js/header.js
@@ -1,26 +1,3 @@
-// When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function () {
-  scrollFunction()
-};
-let headerHeight = $("#header").outerHeight();
-$(".main__content").css({
-  "margin-top": headerHeight + "px"
-})
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    $("#header").css({
-      "font-size": "16px"
-    })
-  } else {
-    $("#header").css({
-      "font-size": "24px"
-    })
-  }
-}
-
-scrollFunction();
-
 $("#btn-menu-toogle").on("click", function (e) {
   var menuToogleIcon = $(this).find("span.icon");
   var navMenu = $("ul#top-menu");
