fd87e2bed02ebeefeca208e36c727036ed2f4092
thanhd <thanhdanh@icd-vn.com>
2022-06-14 12:17:01 +0700

    change menu to mega

--- client/src/js/header.js
+++ client/src/js/header.js
@@ -1,12 +1,3 @@
-
-$("#btn-menu-toogle").on("click", function (e) {
-  var menuToogleIcon = $(this).find("span.icon");
-  var navMenu = $("ul#top-menu");
-  menuToogleIcon.toggleClass("icon-navigation-menu icon-close");
-  navMenu.toggleClass("hidden show");
-})
-$(".btn-search").on("click",function(e){
-  e.preventDefault();
-  $(".search-box").toggleClass("show");
-  $(this).find("span.icon").fadeIn().toggleClass("icon-search icon-close-circled");
+$(".ruby-menu-search:focus").find("a > .icon-search").css({
+  "visibility":"hidden"
 })
